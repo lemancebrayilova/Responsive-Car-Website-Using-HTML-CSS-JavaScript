a1ce02259cc7de3fe92c4150a6354a798d47b906
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-05-29 13:10:52 +0300

    change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,12 @@ const swiperHome = new Swiper('.home__swiper', {
 })
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
